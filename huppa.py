3794584ad9f53f37492efc092e499636ada64f07
Beni Cherniavsky <cben@users.sf.net>
2009-06-26 18:38:06 +0300

    Added figures.

--- huppa.py
+++ huppa.py
@@ -16,11 +16,11 @@ def zip(*lists):
 # Pyjamas doesn't support + operator overloading!
 # + on anything other than numbers returns concatenation of their
 # string representations, e.g.: [1]+[2,3]=="[1][2,3]".
-def add_tuples(*tuples):
+def concat(*tuples):
     res = []
     for t in tuples:
         res.extend(t)
-    return tuple(res)
+    return res
 
 class Cuboid(object):
     """
@@ -32,15 +32,51 @@ class Cuboid(object):
             self.lines = []
         else:
             # Recursive construction: 2 facets + lines between them.
-            facet0 = Cuboid(sizes[1:], add_tuples(fixed_axes, (-sizes[0],)))
-            facet1 = Cuboid(sizes[1:], add_tuples(fixed_axes, (+sizes[0],)))
+            facet0 = Cuboid(sizes[1:], concat(fixed_axes, (-sizes[0],)))
+            facet1 = Cuboid(sizes[1:], concat(fixed_axes, (+sizes[0],)))
 
-            self.points = add_tuples(facet0.points, facet1.points)
-            self.lines = add_tuples(facet0.lines,
+            self.points = concat(facet0.points, facet1.points)
+            self.lines = concat(facet0.lines,
                                     facet1.lines,
                                     zip(facet0.points, facet1.points))
 
+def scale(point, factors):
+    coords = []
+    for (coord, factor) in zip(point, factors):
+        coords.append(coord * factor)
+    return coords    
 
+def figures(legsh=0.2, legsv=0.7, torsov=0.6,
+            handh=0.7, handv0=0.2, handv1=0.3,
+            headv=0.2, headh=0.2,
+            size=1, fixed_axes=(0, 0)):
+    """A schematic drawing of us on a 2D canvas"""
+    # groom: x in [-1,0], y in [TODO: 0,1]  (will scale & mirror later...)
+    gx = -handh
+    gleftfoot = (gx - legsh, -1)
+    grightfoot = (gx + legsh, -1)
+    glowtorso = (gx, -1 + legsv)
+    hightorso = glowtorso[1] + torsov
+    ghightorso = (gx, hightorso)
+    ghandlow = (0, hightorso - handv0)
+    ghandhigh = (0, hightorso - handv1)
+    gheadtop = (gx + headh, hightorso + headv)
+    g = [(gleftfoot, glowtorso), (grightfoot, glowtorso),
+         (glowtorso, ghightorso), (ghightorso, gheadtop),
+         (ghightorso, ghandlow), (ghightorso, ghandhigh)]
+
+    # List comprehensions don't work in pyjamas.
+    lines = []
+    for p0, p1 in g:
+        # 2 figures
+        for scaling in [(size, size), (-size, size)]:
+            s0 = scale(p0, scaling)
+            s1 = scale(p1, scaling)
+            lines.append((concat(s0, fixed_axes),
+                          concat(s1, fixed_axes)))
+    return lines
+
+    
 class LinesCanvas(Canvas):
 
     def __init__(self, w, h):
@@ -48,6 +84,7 @@ class LinesCanvas(Canvas):
 
         # center coordinates on (0,0); scaling will be done on-demand
         self.context.translate(w/2, h/2)
+        # TODO: can we use context.scale to [-1,1] range?
         self.w = w
         self.h = h
 
@@ -63,17 +100,18 @@ class LinesCanvas(Canvas):
         # scale to leave small margin
         scale = 0.9 * min(self.w / 2 / max(xs),
                           self.h / 2 / max(ys))
-        print 'scale =', scale
+        
         # draw
         for line in lines:
             p0, p1 = line
             x0, y0 = transform(p0)
             x1, y1 = transform(p1)
-            print '(%s,%s)-(%s,%s)' % (x0, y0, x1, y1)
-            self.context.moveTo(x0 * 100, y0 * 100)
-            self.context.lineTo(x1 * 100, y1 * 100)
+            # use math coords: y should grow upwards
+            self.context.moveTo(x0 * scale, -y0 * scale)
+            self.context.lineTo(x1 * scale, -y1 * scale)
             self.context.stroke()
-
+        
+        
 class Project2D(object):
     """
     Ad-hoc aphine projection onto XY plane.
@@ -99,5 +137,6 @@ class Project2D(object):
 if __name__ == '__main__':
     canvas = LinesCanvas(300, 300)
     RootPanel().add(canvas)
-    projection = Project2D((0, 0.5, 10), (0, 0, 40))
-    canvas.draw(Cuboid([1, 1, 1, 1]).lines, projection.transform)
+    projection = Project2D((0, 0.5, 4), (0, 0, 20))
+    canvas.draw(concat(concat(Cuboid([1, 1, 1, 1]).lines, figures())),
+                projection.transform)
