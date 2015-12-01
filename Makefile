28d1f3542f4ea84651a9a5166249d95cd785fa9c
Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>
2015-12-01 16:08:03 +0200

    Replaced Pyjamas with Brython. Seriously slower but much cleaner for archival: - no build! - no browser sniffing  (TODO I do have `if lte IE 8` in huppa.txt) - python 3 - APIs closer to raw JS
    Dropped excanvas (which I never got to work?), might work with IE 9+.
    Dropped ClickTale tracking (never worked - now discovered why: docutils XML-escaped inside the script).

--- Makefile
+++ Makefile
@@ -1,13 +1,8 @@
-all: debug
+all: html copy
 
 # Prerequisites needed to build
 # =============================
-pyjamas-trunk:
-	svn co https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk pyjamas-trunk
-
-export PYTHONPATH = ${HOME}/Working/docutils/docutils
-RST2HTML = ~/Working/docutils/docutils/tools/rst2html.py
-#RST2HTML = $(shell which rst2html || echo rst2html-NOT-FOUND)
+RST2HTML = $(shell which rst2html || echo rst2html-NOT-FOUND)
 ${RST2HTML}:
 	@echo "Get Docutils from http://docutils.sf.net"
 	@false
@@ -15,30 +10,21 @@ ${RST2HTML}:
 hibidi.py:
 	curl -O -L http://cben-hacks.sf.net/bidi/hibidi.py
 
-# Config
-# ======
-PYJAMAS = ./pyjamas-trunk
-PYJSFLAGS_DEBUG = --print-statements --strict --line-tracking --source-tracking --store-source
-# -O worked for some time, now doesn't ("Script error. at line number 0. Please inform webmaster").
-PYJSFLAGS_RELEASE = --no-print-statements
-COPY_FILES = figures.png huppa.html ../hazmana.jpg ../hazmana2.jpg excanvas/excanvas.js
+html: output/huppa.html
 
-html: huppa.html
+output/:
+	mkdir output
 
-huppa.html: huppa.txt ru-he.css docutils.conf ${RST2HTML} hibidi.py
-	${RST2HTML} huppa.txt | python ./hibidi.py > huppa.html
-	-cp huppa.html output/
+COPY_FILES = huppa.py figures.png hazmana.jpg hazmana2.jpg
 
-debug: html ${PYJAMAS}
-	${PYJAMAS}/bin/pyjsbuild ${PYJSFLAGS_DEBUG} huppa
+copy: output/
 	cp ${COPY_FILES} output/
 
-release: clean html ${PYJAMAS}
-	${PYJAMAS}/bin/pyjsbuild ${PYJSFLAGS_RELEASE} huppa
-	cp ${COPY_FILES} output/
+output/huppa.html: output/ huppa.txt ru-he.css docutils.conf ${RST2HTML} hibidi.py
+	${RST2HTML} huppa.txt | python ./hibidi.py > output/huppa.html
 
 upload:
 	rsync --progress --compress --delete -r output/ cben@web.sf.net:public_html/huppoid
 
 clean:
-	rm -rf huppa.html output
+	rm -rf output/
--- huppa.py
+++ huppa.py
@@ -1,8 +1,16 @@
-"""
-Render a Schlegel diagram of a 4-D Huppa.
-"""
+# This is a Brython script, which means roughly Python 3.
 
-import pyjd  # noop in pyjs
+"""
+Render a Schlegel diagram (outer cube connected to inner cube) of a 4-D Huppa.
+"""
+print("Is this thing on?")
+
+import math
+
+# Brython
+import browser
+import browser.html as html
+import javascript
 
 # Compatibility stubs
 # ===================
@@ -18,22 +26,28 @@ def zip(*lists):
 def concat(*seqs):
     return [item for seq in seqs for item in seq]
 
+
 # Geometric model
 # ===============
 
+class Point(tuple):
+    """A point in some number of dimensions.
+    Sometimes will have a .z_order attribute added."""
+    pass
+
 class Line(list):
+    """A sequence of points, with some style attributes.  Each point is a list of coordinates.
+    (Use as many coordinates as the dimensions you work in.)"""
     cap = 'round'
     color = 'black'
     mode = 'source-over'
     width = 10  # highlight mistakes
 
 def style_lines(lines, width):
-    lines = map(Line, lines)
+    lines = [Line(l) for l in lines]
     for line in lines:
         line.width = width
     return lines
-
-import math
 
 def interpolate(p0, p1, ratio):
     """
@@ -48,15 +62,16 @@ class Cuboid(object):
     """
     def __init__(self, sizes, fixed_axes={}):
         """
-        `sizes` and `fixed_axes` are dicts.
-        Their keys together should be range(N).
+        `sizes` is list of (axis, size) pairs, `fixed_axes` is {axis: value} dict.
+        In both, axis is an int; all axes taken together should == set(range(N)).
+        This is done so Huppoid can special-case Y axis easier.
         """
         if len(sizes) == 0:
             # A 0-dimentional cuboid = a single point.
             self.lines = []
             # convert dict to list.
-            point = [fixed_axes[axis]
-                     for axis in range(len(fixed_axes.keys()))]
+            point = Point([fixed_axes[axis]
+                           for axis in range(len(fixed_axes.keys()))])
             self.points = [point]
         else:
             # Recursive construction: 2 facets + lines between them.
@@ -79,24 +94,33 @@ class Huppoid(Cuboid):
     4-dimentional cuboid with tweaks.
     """
     def __init__(self, sizes):
+        """`sizes` is list of (axis, size) pairs.
+        First pair is taken to be the non-trivial Y axis.
+        """
+        print("Huppoid()")
         # Recursive construction: 2 facets + lines between them.
         (y_axis, y_size) = sizes[0]
         facet0 = Cuboid(sizes[1:], {y_axis: -y_size})
         facet1 = Cuboid(sizes[1:], {y_axis: +y_size})
+        print("  2 Cuboids computed.")
         self.points = concat(facet0.points, facet1.points)
 
         self.lines = []
         # bottom cube - make dashed lines
         for line in facet0.lines:
-            self.lines.extend(style_lines(self.dash(line), 2))
-        self.lines.extend(style_lines(zip(facet0.points, facet1.points), 4))
+            self.lines.extend(style_lines(self.dash(line), width=2))
+        print("  added dashed.")
+
+        self.lines.extend(style_lines(zip(facet0.points, facet1.points), width=4))
+        print("  added vertical.")
 
         # add a pretty wavy fabric
         for line in facet1.lines:
             wavy = self.wavy(line, y_axis)
             # the stright line should obscure the wavy fabric
-            self.lines.extend(style_lines([line], 5))
-            self.lines.extend(style_lines(wavy, 3))
+            self.lines.extend(style_lines([line], width=5))
+            self.lines.extend(style_lines(wavy, width=3))
+        print("  added wavy.")
 
     def dash(self, line, segments=9):
         p0, p1 = line
@@ -117,12 +141,13 @@ class Huppoid(Cuboid):
         return [wavy]
 
 
-class Point(tuple):
-    pass
-
 class Project2D(object):
     """
     Perspective projection onto XYZ space, then onto XY plane.
+    I'd hoped there is a "natural" 4D->2D projection that looks like I want,
+    but it seems no, I need first to project into 3D and then —
+    from a different point! — into 2D.
+    Which is known as "Schlegel diagram".
     """
 
     def __init__(self, camera):
@@ -155,34 +180,30 @@ class Project2D(object):
 # Canvas interface
 # ================
 
-from pyjamas import Window
-from pyjamas.ui.RootPanel import RootPanel
-from pyjamas.Canvas2D import Canvas, CanvasImage
-from pyjamas.ui.Label import Label
-from pyjamas.ui.TextBox import TextBox
-from pyjamas.ui.VerticalPanel import VerticalPanel
-from pyjamas.ui.Button import Button
-import math
-
-class LinesCanvas(Canvas):
+class LinesCanvas(html.CANVAS):
 
     def __init__(self, w, h):
-        Canvas.__init__(self, w, h)
+        html.CANVAS.__init__(self, width=w, height=h)
+        # fallback content
+        self <= "Your browser doesn't support the <CANVAS> tag.  Try Firefox or Chrome?"
+
         self.w = w
         self.h = h
 
-        context = self.context
+        self.context = context = self.getContext('2d')
         # center coordinates on (0,0); scaling will be done on-demand
         context.translate(w/2, h/2)
         # use math coords: y should grow upwards, not downwards
         context.scale(1, -1)
 
-    def draw(self, transform, lines, points, image):
+    def draw(self, transform, lines, points, figures_coords_and_img):
+        print("draw()")
         context = self.context
 
         context.clearRect(-self.w/2, -self.h/2, self.w, self.h)
+        print("  clearRect.")
 
-        # transform, add white lines slightly behind real lines
+        # Transform, add white lines slightly behind real lines.
         lines2d = []
         for line in lines:
             black_line = Line(map(transform, line))
@@ -202,8 +223,9 @@ class LinesCanvas(Canvas):
             white_line.color = 'white'
             white_line.mode = 'destination-out'
             lines2d.append(white_line)
+        print("  created white and black 2d lines, total", len(lines2d), ".")
 
-        # find bounding square (centered around 0,0)
+        # Find bounding square (centered around 0,0).
         xs = [abs(x)
               for line in lines2d
               for (x, y) in line]
@@ -213,13 +235,20 @@ class LinesCanvas(Canvas):
         # scale to leave small margin
         scale = 0.9 * min(self.w / 2 / max(xs),
                           self.h / 2 / max(ys))
+        print("  computed scale =", scale, ".")
 
-        # draw lines
+        # Draw lines.
         def z_order(line):
             zs = [p.z_order for p in line]
             return (min(zs), max(zs))
-
-        lines2d.sort(keyFunc=z_order)  # key= in normal python
+        # ``lines2d.sort(key=z_order)`` takes 14sec under Brython 3.2.3!
+        # z_order() runs twice per comparison :-(
+        # This decorate-sort-undecorate takes <1sec by only running z_order() once per line.
+        print("  sort()")
+        lines2d_sortkeys = [(z_order(line), i) for (i, line) in enumerate(lines2d)]
+        lines2d_sortkeys.sort()
+        lines2d = [lines2d[i] for (unused, i) in lines2d_sortkeys]
+        print("  computed and z-sorted. drawing...")
 
         context.lineJoin = 'round'
         for line in lines2d:
@@ -232,28 +261,25 @@ class LinesCanvas(Canvas):
             p0 = line[0]
             context.moveTo(p0[0] * scale, p0[1] * scale)
             for p1 in line[1:]:
-##                if extra_width > 0:
-##                    # YIKES :(
-##                    orig0, orig1 = p0, p1
-##                    if orig1 == line[1]:  # first
-##                        p0 = interpolate(orig0, orig1, 0.1)
-##                        context.moveTo(p0[0] * scale, p0[1] * scale)
-##                    if orig1 == line[-1]: # last
-##                        p1 = interpolate(orig0, orig1, 0.9)
                 context.lineTo(p1[0] * scale, p1[1] * scale)
                 p0 = p1
             context.stroke()
+        print("  drew lines")
 
-        # draw points
+        # Draw points.
+        # No white points underneath them => no need for z-order, black is additive.
+        # TODO: This is not strictly true in point-lines and point-image interactions!
         context.fillStyle = 'black'
         for p in points:
             x, y = transform(p)
             context.beginPath()
             context.arc(x * scale, y * scale, 5, 0, math.pi * 2, False)
             context.fill()
+        print("  drew points")
 
-        # draw image
-        x0, y0, x1, y1, img = image
+        # Draw image.
+        # TODO: Always on top is wrong, should observe z-order.
+        x0, y0, x1, y1, img = figures_coords_and_img
         # In math coords (Y grows up), the image should have negative height.
         # Firefox doesn't support negative height.
         # => Work around by temporarily returning to Y-down coords.
@@ -264,69 +290,108 @@ class LinesCanvas(Canvas):
             # This fails with a mysterious NS_ERROR_NOT_AVAILABLE error
             # if the image is not already in cache (at least on Firefox).
             # Attempt to continue instead of dying horribly - doesn't work(?).
-            if img.isLoaded():
-                context.drawImage(img.getElement(),
-                                  x0 * scale, y0 * scale,
-                                  (x1 - x0) * scale, (y1 - y0) * scale)
-        except Exception, e:
-            print e
+            context.drawImage(img,
+                              x0 * scale, y0 * scale,
+                              (x1 - x0) * scale, (y1 - y0) * scale)
+        except Exception as e:
+            print(e)
         context.restore()
+        print("  drew img")
 
-class Main(VerticalPanel):
+# Load JS image objects (bug in Brython 3.0 : can't load IMG object directly wthout using a javascript.JSConstructor object) -- http://webswap.free.fr/brythonrocks/simple.html
+# Was known to not work in OSX Safari?
+Image = javascript.JSConstructor(browser.window.Image)
+
+class Main(html.DIV):
     def __init__(self):
-        VerticalPanel.__init__(self)
+        html.DIV.__init__(self)
         self.canvas = LinesCanvas(500, 500)
-        self.add(self.canvas)
+        self <= self.canvas
+
+        # Start img fetch early, specifically before we compute the Huppoid.
+        self.figures_img = Image()
+        self.figures_img.src = 'figures.png'
 
         self.boxes = []
         for axis, c in zip("xyzw", (0, 1.7, 6, 20)):
-            self.add(Label("Camera %s:" % axis))
-            box = TextBox()
-            box.setText(c)
+            box = html.INPUT(type='number', step='0.5')
+            box.value = str(c)
+            box.bind('keyup', lambda ev: self.draw())
+            box.bind('input', lambda ev: self.draw())
             self.boxes.append(box)
-            self.add(box)
-            box.addKeyboardListener(self)
+            self <= html.DIV(html.LABEL("Camera %s:" % axis) + box)
         self.camera = None  # force first redraw
         # first axis is Y - direction of huppoid
         # second axis is Z for correct Z-order.
         self.huppoid = Huppoid([(1,1), (2,1), (3,1), (0,1)])
-        self.figures = CanvasImage('figures.png')
-        self.figures.addLoadListener(DrawOnLoad(self))
-##        self.draw()
 
     def draw(self):
-        camera = [float(box.getText())
+        camera = [float(box.value)
                   for box in self.boxes]
         # guard against spurious redrawing (e.g. arrows or Tab presses)
         if camera != self.camera:
+            if not self.figures_img.complete:
+                print("postponing draw() until img loads")
+                self.figures_img.bind('load', lambda ev: self.draw())
+                return
+
+            print("(re)drawing for camera =", camera)
             self.camera = camera
             self.projection = Project2D(camera)
             self.canvas.draw(self.projection.transform,
                              self.huppoid.lines,
                              self.huppoid.points,
-                             # assume source image is square
-                             (-.8, -1, .8, .6, self.figures))
+                             # Bottom center (assume source image is square).
+                             (-0.8, -1.0, 0.8, 0.6, self.figures_img))
 
+def debug():
+    """Poor man's python console.
+    Useful because browser's JS console doesn't give sane access to Brython objects.
+    """
+    import traceback
+    prompt = html.INPUT(type='text', size=80, style={'font-family': 'monospace'})
+    output = html.PRE()
+    def rep():
+        # Our console overwrites outputs in-place, so also dump full "history" into browser's console.
+        print('>>> ' + prompt.value)
+        try:
+            try:
+                code = compile(prompt.value, "<debug input>", 'eval')
+            except SyntaxError:
+                code = compile(prompt.value, "<debug input>", 'exec')
+            value = eval(code)
+            # Native JS objects (e.g. DOM nodes) are reflected as having no __repr__
+            # but all seem support str() via toString().
+            try:
+                res = repr(value)
+            except:
+                res = str(value)
+        except:
+            res = traceback.format_exc()
+        output.text = res
+        output.scrollIntoView({'block': 'end'})
+        print(res)
+    # 'change' fires when you press Enter (or change focus)
+    prompt.bind('change', lambda ev: rep())
+    browser.document <= html.DIV(html.HR() +
+                                 html.DIV(">>> " + prompt) +
+                                 output +
+                                 html.HR())
 
-    def onKeyUp(self, sender, keyCode, modifiers):
-        self.draw()
-
-    def onKeyDown(self, sender, keyCode, modifiers):
-        pass
-
-    def onKeyPress(self, sender, keyCode, modifiers):
-        pass
-
-class DrawOnLoad(object):
-    def __init__(self, obj):
-        self.obj = obj
-
-    def onLoad(self, x):
-        self.obj.draw()
+l = [1.0, 2.0, 3.0]
+p = Point(l)
+q = list(p)
+print(l, p, q, q is p)
+x = l[0]
+x += 10
+y = p[1]
+y += 20
+z = q[2]
+z += 30
+print(l, p, q, p is l, q is p)
 
 if __name__ == '__main__':
-    pyjd.setup("./output/huppa.html")
-
-    RootPanel().add(Main())
-
-    pyjd.run()
+    main = Main()
+    browser.document <= main
+    main.draw()
+    debug()
