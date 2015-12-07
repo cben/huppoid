0afe47e75004efcb584e0583376f1edd4d10b851
Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>
2015-12-07 19:13:46 +0200

    Better but still imperfect z-order.  Document rationale and why it's hard. Mildly broken example: huppa.html?debug=1#-4.0,3.0,3.0,8.0

--- huppa.py
+++ huppa.py
@@ -78,8 +78,8 @@ class FlatImage():
         self.corner1 = corner1
 
     def draw_on_canvas(self, context, scale):
-        x0, y0, z, w = self.corner0
-        x1, y1, z, w = self.corner1
+        x0, y0 = self.corner0
+        x1, y1 = self.corner1
 
         # In math coords (Y grows up), the image should have negative height.
         # Firefox doesn't (didn't in 2009?) support negative height.
@@ -198,7 +198,8 @@ class Project2D(object):
     def __init__(self, camera):
         self.camera = camera
 
-    def transform(self, point):
+    def transform_point(self, point):
+        """Transform a 4D point into a 2D point."""
         xc, yc, zc, wc = self.camera
         x, y, z, w = point
         # translate relative to (0, 0, 0, wc) camera
@@ -217,10 +218,87 @@ class Project2D(object):
         # translate back to be relative to XY origin
         x += xc
         y += yc
+        z += zc
 
         res = Point((x, y))
-        res.z_order = z
+        res.z = z  # For z-order computations
         return res
+
+    # Z-Order (Painter's algorithm) is a lie
+    # --------------------------------------
+    #
+    # First, the goals.  The *only* reason drawing order matters is that
+    # I'm drawing white lines under black lines to create a
+    # front-obscures-back effect: -|-
+    #
+    # I also want corners with a fat point to look "clean" with points not
+    # obscured by any of the meeting lines - even back points by lines
+    # coming from front.  However, unrelated lines in front should obscure
+    # points.  Sounds tricky, but that's not the hard part.
+    #
+    # Mistake 1: I'd thought that since my projection is always onto a fixed-Z
+    # plane, I can order objects solely by their absolute Z coordinate (after
+    # 4D->3D transform, but disregarding camera location).  I can't, because when
+    # camera looks from the left, the left side should obscure the right part
+    # (with exactly same Z coordinates), and vice versa.
+    #
+    # Mistake 2: Expecting I just need a better formula for an object's z-order, e.g.
+    # distance from camera to infinite continuation of a line (doesn't work).
+    #
+    # Mistake 3: Expecting I can at least write a pairwise comparison function.
+    # In *general position*, lines obsrcuring might not be transitive!
+    # You can lay sticks like this:
+    #      |  |
+    #     ----|-
+    #      |  |
+    #     -|----
+    #      |  |
+    # (That's why per-pixel Z-buffer got invented!  Hard to implement with Canvas.)
+    #
+    # => Can I still get away with a (almost correct) lie for Huppoid?!?
+    #
+    # Unfortunately I don't have time to debug this further, so here is my
+    # heuristic from 2009:
+    #
+    # 1. Sort lines by (farthest_point, closest_point).  This way front lines
+    #    obscure back-front connecting lines which obscure back lines.  The order
+    #    of the 2 is not very important for lines [*]_, but having farthest first
+    #    allows a fat point at line's back end to still not be obscured by white line
+    #    whose depth starts with farthest_point + epsilon.  Kludge...
+    #
+    #    .. [*] It is important outer vs inner line crossings, but either way gets
+    #       some of them wrong.
+    #
+    # with a mild improvements:
+    #
+    # 2. Sort points as (depth, depth) for consistency, and the image as
+    #    (farthest_corner, closest_corner).  [In 2009 I always drew points and
+    #    image after lines.]
+    #
+    # 3. Measure point depth by (z, -pythagorean distance) to camera.  Distance
+    #    to camera gets the right-vs-left depending on camera position part
+    #    mostly right, and *mostly* doesn't cause other bugs.  (z, -distance) causes
+    #    slightly less bugs.
+
+    # These are methods of `Project2D` so they know where the camera is.
+
+    def point_depth(self, point):
+        """Pythagorean distance to camera."""
+        x, y = point
+        z = point.z
+        xc, yc, zc, wc = self.camera
+        distance_to_camera = ((x - xc)**2 + (y - yc)**2 + (z - zc)**2) #**0.5 -- same order
+        return ((z, -distance_to_camera), (z, -distance_to_camera))
+
+    def line_depth(self, line):
+        ds = [self.point_depth(p)[0] for p in line]
+        # (min, max) order ensures front-back connecting lines are
+        # drawn after back and before front.
+        return (min(ds), max(ds))
+
+    def image_depth(self, image):
+        # TODO: all 4 corners
+        return self.line_depth([image.corner0, image.corner1])
 
 
 # Canvas interface
@@ -242,7 +320,7 @@ class LinesCanvas(html.CANVAS):
         # use math coords: y should grow upwards, not downwards
         context.scale(1, -1)
 
-    def draw(self, transform, lines, points, images):
+    def draw(self, projection, lines, points, images):
         print("draw()")
         context = self.context
 
@@ -254,12 +332,13 @@ class LinesCanvas(html.CANVAS):
         # of the points are intermediate points on dashed/wavy lines;
         # we have 460 unique points and do about 548 transforms total,
         # so optimizing it is hardly worth it.
+        transform_point = projection.transform_point
 
         # Transform lines, add wider white lines slightly behind real lines
-        # to create front-obscures-back effect at crossings.
+        # to create front-obscures-back effect at crossings: -|-
         lines2d = []
         for line in lines:
-            black_line = Line(map(transform, line))
+            black_line = Line(map(transform_point, line))
             black_line.width = line.width
             black_line.cap = line.cap
             black_line.color = line.color
@@ -269,7 +348,7 @@ class LinesCanvas(html.CANVAS):
             white_line = Line()
             for black_p in black_line:
                 white_p = Point(black_p)
-                white_p.z_order = black_p.z_order - 0.05
+                white_p.z = black_p.z - 0.05  # a tiny bit behind
                 white_line.append(white_p)
             white_line.width = line.width + 5
             white_line.cap = 'butt'
@@ -278,37 +357,24 @@ class LinesCanvas(html.CANVAS):
             lines2d.append(white_line)
         print("  created white and black 2d lines, total", len(lines2d), ".")
 
-        points2d = list(map(transform, points))
+        points2d = list(map(transform_point, points))
 
-        images2d = []
-        for image in images:
-            image2d = FlatImage(image.img_element,
-                                transform(image.corner0),
-                                transform(image.corner1))
-            assert image2d.corner0.z_order == image2d.corner1.z_order
-            image2d.z_order = image2d.corner0.z_order
-            images2d.append(image2d)
-
-        # objects = lines2d + points2d + images2d
+        images2d = [FlatImage(image.img_element,
+                              transform_point(image.corner0),
+                              transform_point(image.corner1))
+                    for image in images]
 
         print("  sort()")
-        def line_z_order(line):
-            zs = [p.z_order for p in line]
-            # (min, max) order ensures front-back connecting lines are
-            # drawn after back and before front.
-            return (min(zs), max(zs))
-
-        # ``lines2d.sort(key=line_z_order)`` takes 14sec under Brython 3.2.3!
+        # ``lines2d.sort(key=projection.line_depth)`` takes 14sec under Brython 3.2.3!
         # z_order() runs twice per comparison :-(
         # decorate-sort-undecorate takes ~1sec by only running z_order() once per line.
-        # Use a 2-tier group, index scheme for resolving back the objects.
-        global sortkeys, lines2d, points2d
-        sortkeys = ([(line_z_order(line), 'line', i) for i, line in enumerate(lines2d)] +
-                    [((point.z_order, point.z_order), 'point', i) for i, point in enumerate(points2d)] +
-                    [((image.z_order, image.z_order), 'image', i) for i, image in enumerate(images2d)])
-        sortkeys.sort()
-        groups = {'line': lines2d, 'point': points2d, 'image': images}
-        objects = [groups[group][i] for (unused, group, i) in sortkeys]
+        depths_objects = ([(projection.line_depth(l), l) for l in lines2d] +
+                          [(projection.point_depth(p), p) for p in points2d] +
+                          [(projection.image_depth(i), i) for i in images2d])
+        global objects, tosort, lines2d, points2d, images2d  # DEBUG
+        tosort = [(depth, i) for (i, (depth, obj)) in enumerate(depths_objects)]
+        tosort.sort()
+        objects = [depths_objects[i][1] for (unused, i) in tosort]
         print("  z-sorted.")
 
         # Find bounding square (centered around 0,0).
@@ -354,9 +420,10 @@ class Main(html.DIV):
         browser.window.onhashchange = self.onhashchange
         self.last_camera = None  # first draw() should always draw.
 
-        # first axis is Y - direction of huppoid
-        # second axis is Z for correct Z-order.
-        self.huppoid = Huppoid([(1,1), (2,1), (3,1), (0,1)])
+        # 4D coordinates are always in XYZW order (that's what Project2D assumes).
+        # Huppoid constructor wants (axis, size) pairs where the first axis
+        # should be the non-symmetric base-roof axis, i.e. Y = 1.
+        self.huppoid = Huppoid([(1, 1.0), (2, 1.0), (3, 1.0), (0, 1.0)])
 
     def onhashchange(self, event):
         self.set_boxes_from_hash()
@@ -365,8 +432,8 @@ class Main(html.DIV):
     def set_boxes_from_hash(self):
         if ',' in browser.window.location.hash:
             camera = map(float, browser.window.location.hash.lstrip('#').split(','))
-        for box, c in zip(self.boxes, camera):
-            box.value = str(c)
+            for box, c in zip(self.boxes, camera):
+                box.value = str(c)
 
     def onedit(self, event):
         self.draw()
@@ -386,7 +453,7 @@ class Main(html.DIV):
             print("(re)drawing for camera =", camera)
             self.last_camera = camera
             self.projection = Project2D(camera)
-            self.canvas.draw(self.projection.transform,
+            self.canvas.draw(self.projection,
                              self.huppoid.lines,
                              self.huppoid.points,
                              [self.image])
@@ -426,12 +493,12 @@ def debug():
                                  output +
                                  html.HR())
 
+
 if __name__ == '__main__':
+    if browser.document.query.getfirst('debug') is not None:
+        debug()
     main = Main()
     target = browser.document.getElementById('huppoid_here')
     target.html = ''  # replace any fallback content
     target <= main
     main.draw()
-
-    if browser.document.query.getfirst('debug') is not None:
-        debug()
